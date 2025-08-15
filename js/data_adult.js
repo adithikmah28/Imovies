de7b0e106368fc1815cd503a9eac12794501a707
BioskopBoxOffice <hikmahtiaraditya@gmail.com>
2025-08-15 14:26:05 +0700

    Update data_adult.js

--- js/data_adult.js
+++ js/data_adult.js
@@ -1,33 +1,30 @@
 // =======================================================
-//   DATABASE FILM UNTUK HALAMAN ADULT (VERSI LENGKAP)
+//   DATABASE FILM UNTUK HALAMAN ADULT (DENGAN TAGS)
 // =======================================================
 
 const adultMoviesData = [
     {
         id: 1,
-        title: "MFC-343 Shion",
-        posterUrl: "https://avdb.stream27.com/v/MFC-343/thumb.jpg", // Mengganti thumbnailUrl menjadi posterUrl
-        iframeSrc: "https://upload18.com/play/index/mfc-343", // Link video embed
-        
-        // --- INFORMASI DETAIL BARU ---
-        idCode: "MFC-343 Shion",
-        categories: ["Big Ass", "Creampie", "Oral Sex", "Massage Oil", "Selfie", "Super Breasts", "Hd", "Big Breasts", "Sister", "Black Hair", "4K"],
-        actors: ["Unknown"],
-        year: "2025",
-        country: "Japan",
+        title: "[SUB-INDO] Galactic Odyssey: The Final Frontier",
+        posterUrl: "https://image.tmdb.org/t/p/w500/A4jG0t0F3n4kQd3gldAlOa0d8os.jpg",
+        iframeSrc: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+        idCode: "GAL-001-sub-indo",
+        tags: ["Censored", "English Subtitle", "Indonesia Subtitle"], // <-- TAMBAHKAN TAGS DI SINI
+        actors: ["Chris Pratt", "Zoe Saldaña", "Dave Bautista"],
+        year: "2023",
+        country: "USA",
         director: "James Gunn",
-        writer: "MOON FORCE",
-        duration: "1:19:42",
-        releaseDate: "2025-08-13"
+        writer: "James Gunn",
+        duration: "02:15:30",
+        releaseDate: "2023-05-05"
     },
     {
         id: 2,
         title: "[SUB-INDO] Cyber City Chronicles: Neon Shadow",
         posterUrl: "https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg",
         iframeSrc: "https://vidsrc.to/embed/movie/tt0133093",
-        
         idCode: "CYBER-002-sub-indo",
-        categories: ["Cyberpunk", "Action", "Thriller"],
+        tags: ["Uncensored", "English Subtitle", "Chinese AV"], // <-- CONTOH TAGS
         actors: ["Keanu Reeves", "Carrie-Anne Moss"],
         year: "1999",
         country: "USA",
@@ -41,9 +38,8 @@ const adultMoviesData = [
         title: "[SUB-INDO] The Last Sorcerer's Heir",
         posterUrl: "https://image.tmdb.org/t/p/w500/9O7gL8sri36c035n62OXLv5p2v2.jpg",
         iframeSrc: "https://vidsrc.to/embed/movie/tt0120737",
-        
         idCode: "SORC-003-sub-indo",
-        categories: ["Fantasy", "Adventure", "Magic"],
+        tags: ["Amateur", "Indonesia Subtitle"], // <-- CONTOH TAGS
         actors: ["Elijah Wood", "Ian McKellen", "Viggo Mortensen"],
         year: "2001",
         country: "New Zealand",
@@ -52,21 +48,19 @@ const adultMoviesData = [
         duration: "02:05:45",
         releaseDate: "2001-12-19"
     },
-    // --- CONTOH TAMBAH FILM BARU ---
-    // {
-    //     id: 4,
-    //     title: "[SUB-INDO] Judul Film Baru",
-    //     posterUrl: "https://link-gambar-poster.com/poster.jpg",
-    //     iframeSrc: "https://link-embed-video.com/video",
-    //
-    //     idCode: "KODE-FILM-004",
-    //     categories: ["Genre 1", "Genre 2"],
-    //     actors: ["Aktor 1", "Aktris 1"],
-    //     year: "2024",
-    //     country: "Indonesia",
-    //     director: "Sutradara",
-    //     writer: "Penulis",
-    //     duration: "01:30:00",
-    //     releaseDate: "2024-10-26"
-    // },
+    {
+        id: 4,
+        title: "[SUB-INDO] Uncensored Leaked Footage",
+        posterUrl: "https://image.tmdb.org/t/p/w500/s3TBr3jTJB4IuEJVD2G2s2i7T3D.jpg",
+        iframeSrc: "https://vidsrc.to/embed/movie/tt0417435",
+        idCode: "LEAK-004",
+        tags: ["Uncensored Leaked", "Amateur"], // <-- CONTOH TAGS
+        actors: ["Unknown"],
+        year: "2024",
+        country: "Japan",
+        director: "N/A",
+        writer: "N/A",
+        duration: "00:45:10",
+        releaseDate: "2024-01-15"
+    },
 ];
